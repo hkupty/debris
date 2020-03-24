cf6d52e1cdce4ff4c89ea04b221c0f0152408d4e
Henry Kupty <hkupty@gmail.com>
2020-03-24 11:32:57 +0100

    Adjusting to spec
    Adding some small tests to it, while also making sizes 4-bytes and fixing deserializing empty sequences

--- src/debris/serde.clj
+++ src/debris/serde.clj
@@ -40,78 +40,95 @@
 ;; Ordered map
 (def ^:private ordered-map-prefix (byte 0x20))
 
+(def max-size 4294967295)
+
 (defmulti serialize type)
 (defmulti deserialize-value first)
 
+(defn size-in-bytes [integer]
+  (cond
+    (neg? integer) (throw (ex-info "Invalid negative size"
+                                   {:reason ::invalid-negative-size
+                                    :supplied-size integer
+                                    :max-size max-size}))
+    (> integer max-size) (throw (ex-info "Unsupported size"
+                                         {:reason ::unsupported-size
+                                          :supplied-size integer
+                                          :max-size max-size}))
+    :else (byte-array [(unchecked-byte (bit-and 0xFF (bit-shift-right integer 24)))
+                       (unchecked-byte (bit-and 0xFF (bit-shift-right integer 16)))
+                       (unchecked-byte (bit-and 0xFF (bit-shift-right integer 8)))
+                       (unchecked-byte (bit-and 0xFF integer))])))
+
+(defn deserialize-size [^bytes barr] (BigInteger. 1 barr))
+
+(comment
+  (vec (.toByteArray (.toBigInteger 256N)))
+  (BigInteger. 1 (byte-array (repeat 4 (unchecked-byte 0xFF))))
+  )
+
 (defn- serialize-byte [raw]
-  (byte-array [byte-prefix (byte 0x1) raw]))
+  (byte-array [byte-prefix (byte 0x00) (byte 0x00) (byte 0x00) (byte 0x1) raw]))
 
 (defn- serialize-bytes [^bytes raw]
-  (byte-array (into
-                  [byte-prefix
-                   (byte (count raw))]
-                  (vec raw))))
+  (byte-array (concat
+                [byte-prefix]
+                (size-in-bytes (count raw))
+                (vec raw))))
 
 (defn- serialize-numbers [number]
- (let [str-repr (str number)]
-    (byte-array (into
-                  [num-prefix
-                   (byte (count str-repr))]
-                  (map byte)
-                  str-repr))))
+  (let [str-repr (str number)]
+    (byte-array (concat
+                  [num-prefix]
+                  (size-in-bytes (count str-repr))
+                  (map byte str-repr)))))
 
 (defn- serialize-text [str-]
-  (byte-array (into
-                [text-prefix
-                 (byte (count str-))]
-                (map byte)
-                str-)))
+  (byte-array (concat
+                [text-prefix]
+                (size-in-bytes (count str-))
+                (map byte str-))))
 
 (defn- serialize-named [symbol-or-kw]
   (let [ns- (namespace symbol-or-kw)
         str-repr (str/join "/"
                            (cond->> [(name symbol-or-kw)]
                              (some? ns-) (cons ns-)))]
-    (byte-array (into
-                  [text-prefix
-                   (byte (count str-repr))]
-                  (map byte)
-                  str-repr))))
+    (byte-array (concat
+                  [text-prefix]
+                  (size-in-bytes (count str-repr))
+                  (map byte str-repr)))))
 
 (defn- serialize-bool [bool]
-  (byte-array [bool-prefix (byte 0x1) (byte (cond-> 0 bool inc))]))
+  (byte-array [bool-prefix (byte 0x0) (byte 0x0) (byte 0x0) (byte 0x1) (byte (cond-> 0 bool inc))]))
 
 (defn- serialize-maps [map-]
   (let [body (sort-by (comp vec first)
                       compare
                       (map (fn [[k v]] [(serialize k) (serialize v)]) map-))
         body (apply concat [] body)
-        size (transduce (map count) + body)
-        header (byte-array [unordered-map-prefix (byte size)])]
-    (byte-array (apply concat [] header body))))
+        size (transduce (map count) + body)]
+    (byte-array (apply concat [unordered-map-prefix] (size-in-bytes size) body))))
 
 (defn- serialize-sets [set-]
   (let [body (sort-by vec
                       compare
                       (map serialize set-))
-        size (transduce (map count) + body)
-        header (byte-array [unordered-seq-prefix (byte size)])]
-    (byte-array (apply concat [] header body))))
+        size (transduce (map count) + body)]
+    (byte-array (apply concat [unordered-seq-prefix] (size-in-bytes size) body))))
 
 (defn- serialize-vecs [seq-]
   (let [body (map serialize seq-)
-        size (transduce (map count) + body)
-        header (byte-array [ordered-seq-prefix (byte size)])]
-    (byte-array (apply concat [] header body))))
+        size (transduce (map count) + body)]
+    (byte-array (apply concat [ordered-seq-prefix] (size-in-bytes size) body))))
 
 (defn- serialize-ordered-kvps [seq-]
   (let [body (mapcat (fn [[k v]] [(serialize k) (serialize v)]) seq-)
-        size (transduce (map count) + body)
-        header (byte-array [ordered-seq-prefix (byte size)])]
-    (byte-array (apply concat [] header body))))
+        size (transduce (map count) + body)]
+    (byte-array (apply concat [ordered-seq-prefix] (size-in-bytes size) body))))
 
 (defmethod serialize java.lang.Byte [data] (serialize-byte data))
-(defmethod serialize  (type (byte-array [])) [data] (serialize-bytes data))
+(defmethod serialize (type (byte-array [])) [data] (serialize-bytes data))
 (defmethod serialize java.lang.String [data] (serialize-text data))
 (defmethod serialize clojure.lang.Keyword [data] (serialize-named data))
 (defmethod serialize clojure.lang.Symbol [data] (serialize-named data))
@@ -138,6 +155,17 @@
 
 (defmethod serialize java.lang.Boolean [data] (serialize-bool data))
 
+(defn deserialize-chunk [chunk-]
+  (if (seq chunk-)
+    (let [[tp & data] chunk-
+          sz (deserialize-size (byte-array (take 4 data)))
+          [payload next-] (split-at sz (drop 4 data))]
+      [(deserialize-value [tp sz payload]) next-])
+    []))
+
+(defn deserialize [data]
+  (first (deserialize-chunk data)))
+
 (defmethod deserialize-value byte-prefix [[_ _ this]] this)
 
 (defmethod deserialize-value num-prefix [[_ _ this]]
@@ -150,53 +178,34 @@
   (into {}
         (map vec)
         (partition 2 (loop [kvps []
-                               [des nxt] (deserialize-chunk this)]
-                          (cond-> (conj kvps des)
-                            (seq nxt) (recur (deserialize-chunk nxt)))))))
+                            [des nxt] (deserialize-chunk this)]
+                       (cond-> kvps
+                         (seq des) (conj des)
+                         (seq nxt) (recur (deserialize-chunk nxt)))))))
 
 (defmethod deserialize-value ordered-map-prefix [[_ _ this]]
   (into (sorted-map)
         (map vec)
         (partition 2 (loop [kvps []
-                               [des nxt] (deserialize-chunk this)]
-                          (cond-> (conj kvps des)
-                            (seq nxt) (recur (deserialize-chunk nxt)))))))
+                            [des nxt] (deserialize-chunk this)]
+                       (cond-> kvps
+                         (seq des) (conj des)
+                         (seq nxt) (recur (deserialize-chunk nxt)))))))
 
 (defmethod deserialize-value unordered-seq-prefix [[_ _ this]]
   (loop [vs #{}
          [des nxt] (deserialize-chunk this)]
-    (cond-> (conj vs des)
+    (cond-> vs
+      (seq des) (conj des)
       (seq nxt) (recur (deserialize-chunk nxt)))))
 
 
 (defmethod deserialize-value ordered-seq-prefix [[_ _ this]]
   (loop [vs []
          [des nxt] (deserialize-chunk this)]
-    (cond-> (conj vs des)
+    (cond-> vs
+      (seq des) (conj des)
       (seq nxt) (recur (deserialize-chunk nxt)))))
 
 (defmethod deserialize-value text-prefix [[_ _ this]]
   (String. (into-array Byte/TYPE this)))
-
-(defn deserialize-chunk [chunk-]
-  (let [[tp sz & data] chunk-
-        [payload next-] (split-at sz data)]
-    [(deserialize-value [tp sz payload]) next-]))
-
-(defn deserialize [data]
-  (first (deserialize-chunk data)))
-
-(comment
-  (into (byte-array [(byte 1)])
-        (byte-array [(byte 2)]))
-  (deserialize (serialize true))
-  (= 0 (compare (vec (serialize {1 2, 3 4, 0 5, 2 3}))
-                (vec (serialize {0 5, 3 4, 2 3, 1 2}))))
-
-  (deserialize (serialize 30.1))
-
-  (deserialize (vec (serialize {::stuff 1 3 true false #{1 2 3 5 0}})))
-
-  (compare [(byte 0x2) (byte 0x8) (byte 0x48) (byte 0x48)]
-           [(byte 0x2) (byte 0x1) (byte 0x50)])
-  )
